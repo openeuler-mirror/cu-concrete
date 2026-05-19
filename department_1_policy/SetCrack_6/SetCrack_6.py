a40b6600438d1852578dae995962757d5e1a3660
caojingbo <caojb22@chinaunicom.cn>
2026-05-19 15:12:18 +0800

    Complete SetCrack_6 with new classes, +23 lines.

--- department_1_policy/SetCrack_6/SetCrack_6.py
+++ department_1_policy/SetCrack_6/SetCrack_6.py
@@ -11,33 +11,35 @@ from base_fix import base_fix
 from base_shell_function import base_shell_function as bsf
 from base_shell import base_shell
 import logging
+# import pandas as pd
 import Panda as pd
 logging.getLogger(__name__)
-
-class SetCrack_6(base_fix):
-
+#TestCase-部门编号-子加固项名称-子加固项编号
+class SetCrack_6(base_fix):    
     def __init__(self):
         super().__init__()
+
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.config_file = os.path.join(self.current_dir, 'SetCrack_6.yaml')
-        with open(file=self.config_file, mode='r+', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
-        self.pkl_file = os.path.join(os.path.dirname(self.current_dir), 'data_status.pkl')
-        self.config = config
-        self.status = None
+        self.config_file = os.path.join(self.current_dir, "SetCrack_6.yaml")
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
+        self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
+        self.config=config
+        self.status=None
 
     def finalfix(self):
-        self.status = 2
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
         self.status = 1
+        # 每次加固前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 1
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 1
         self.status_form.to_pickle(self.pkl_file)
         bsf.touch_shell(self.config['backup_path'])
         bsf.cp_shell(self.config['query']['path'], self.config['backup_path'])
@@ -51,15 +53,16 @@ class SetCrack_6(base_fix):
         data = 'type:fix,des:{}'.format(self.config['description'])
         logging.info(data)
         self.finalfix()
-
+    
     def check(self):
-        except_value = True
-        result = bsf.grep_shell(self.config['change']['value'], self.config['query']['path'])
-        if result[1] != 0:
-            except_value = False
+        except_value=True
+        result=bsf.grep_shell(self.config['change']['value'],self.config['query']['path'])
+        if result[1]!=0:
+            except_value=False
         return except_value
 
     def rollback(self):
+        # 每次还原前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
@@ -67,7 +70,7 @@ class SetCrack_6(base_fix):
         bsf.cp_shell(self.config['backup_path'], self.config['query']['path'])
         result = self.check()
         if result == False:
-            self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 0
+            self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 0
             self.status_form.to_pickle(self.pkl_file)
 
     def reset(self):
@@ -75,5 +78,6 @@ class SetCrack_6(base_fix):
         self.fix()
 
     def get_des(self):
-        description = self.config['description']
+        description=self.config['description']
         return description
+
