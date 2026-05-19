67ca89c6044b91524b396b69080a9168356bef63
caojingbo <caojb22@chinaunicom.cn>
2026-05-19 11:15:46 +0800

    Complete LoginUserLock_7 implementation

--- department_1_policy/LoginUserLock_7/LoginUserLock_7.py
+++ department_1_policy/LoginUserLock_7/LoginUserLock_7.py
@@ -11,65 +11,68 @@ from base_fix import base_fix
 from base_shell_function import base_shell_function as bsf
 from base_shell import base_shell
 import logging
+# import pandas as pd
 import Panda as pd
 logging.getLogger(__name__)
-
-class LoginUserLock_7(base_fix):
-
+#TestCase-部门编号-子加固项名称-子加固项编号
+class LoginUserLock_7(base_fix):    
     def __init__(self):
         super().__init__()
+
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.config_file = os.path.join(self.current_dir, 'LoginUserLock_7.yaml')
-        with open(file=self.config_file, mode='r+', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
-        self.pkl_file = os.path.join(os.path.dirname(self.current_dir), 'data_status.pkl')
-        self.config = config
-        self.status = None
+        self.config_file = os.path.join(self.current_dir, "LoginUserLock_7.yaml")
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
+        self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
+        self.config=config
+        self.status=None
 
     def finalfix(self):
-        self.status = 2
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
         self.status = 1
+        # 每次加固前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
-        bsf.cp_shell(self.config['query']['path'], self.config['query']['path'] + '.bak')
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 1
+        bsf.cp_shell(self.config['query']['path'],self.config['query']['path']+'.bak')
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 1
         self.status_form.to_pickle(self.pkl_file)
         flag = bsf.grep_shell(self.config['query']['form'][0], self.config['query']['path'])
         if len(flag[0]) != 0:
-            cmd = ['sudo', 'sed', '-i', 's/{}/{}/g'.format(self.config['query']['form'][0], self.config['change']['value'][0]), self.config['query']['path']]
+            cmd=['sudo', 'sed', '-i', 's/{}/{}/g'.format(self.config['query']['form'][0],self.config['change']['value'][0]), self.config['query']['path']]
             base_shell(cmd)
         data = 'type:fix,des:{}'.format(self.config['description'])
         logging.info(data)
         self.finalfix()
 
     def check(self):
-        except_value = True
-        result = bsf.grep_shell(self.config['change']['value'][1], self.config['query']['path'])
-        if result[0] != self.config['change']['value'][1]:
-            except_value = False
-        result = bsf.grep_shell(self.config['change']['value'][2], self.config['query']['path'])
-        if result[0] != self.config['change']['value'][2]:
-            except_value = False
-        result = bsf.grep_shell(self.config['query']['form'][3], self.config['query']['path'])
-        if result[0] != self.config['change']['value'][3]:
-            except_value = False
+        except_value=True
+        result=bsf.grep_shell(self.config['change']['value'][1],self.config['query']['path'])
+        if result[0]!=self.config['change']['value'][1]:
+            except_value=False
+        result=bsf.grep_shell(self.config['change']['value'][2],self.config['query']['path'])
+        if result[0]!=self.config['change']['value'][2]:
+            except_value=False
+        result=bsf.grep_shell(self.config['query']['form'][3],self.config['query']['path'])
+        if result[0]!=self.config['change']['value'][3]:
+            except_value=False
         return except_value
 
     def rollback(self):
-        bsf.cp_shell(self.config['query']['path'] + '.bak', self.config['query']['path'])
+        bsf.cp_shell(self.config['query']['path']+'.bak',self.config['query']['path'])
         result = self.check()
+        # 每次还原前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
         if result == False:
-            self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 0
+            self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 0
             self.status_form.to_pickle(self.pkl_file)
 
     def reset(self):
@@ -77,5 +80,5 @@ class LoginUserLock_7(base_fix):
         self.fix()
 
     def get_des(self):
-        description = self.config['description']
+        description=self.config['description']
         return description
