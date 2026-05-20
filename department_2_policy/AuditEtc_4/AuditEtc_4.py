c451af93fe51a7757d97af38aa1c75c42826c517
caojingbo <caojb22@chinaunicom.cn>
2026-05-20 14:43:54 +0800

    Complete AuditEtc_4 implementation

--- department_2_policy/AuditEtc_4/AuditEtc_4.py
+++ department_2_policy/AuditEtc_4/AuditEtc_4.py
@@ -11,57 +11,59 @@ from base_fix import base_fix
 from base_shell_function import base_shell_function as bsf
 from base_shell import base_shell
 import logging
+# import pandas as pd
 import Panda as pd
 logging.getLogger(__name__)
-
-class AuditEtc_4(base_fix):
-
+#TestCase-部门编号-子加固项名称-子加固项编号
+class AuditEtc_4(base_fix):    
     def __init__(self):
         super().__init__()
+
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.config_file = os.path.join(self.current_dir, 'AuditEtc_4.yaml')
-        with open(file=self.config_file, mode='r+', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
-        self.pkl_file = os.path.join(os.path.dirname(self.current_dir), 'data_status.pkl')
-        self.config = config
-        self.status = None
+        self.config_file = os.path.join(self.current_dir, "AuditEtc_4.yaml")
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
+        self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
+        self.config=config
+        self.status=None
 
     def finalfix(self):
-        self.status = 2
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
         self.status = 1
+        # 每次加固前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 1
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=1
         self.status_form.to_pickle(self.pkl_file)
         with open(self.config['query']['path'][1], 'w') as f:
-            f.write(self.config['query']['form'] + '\n')
+            f.write(self.config['query']['form']+"\n")
         bsf.delete_audit_rule()
         bsf.reload_audit_rules()
-        data = 'type:fix,des:{}'.format(self.config['description'])
+        data='type:fix,des:{}'.format(self.config['description'])
         logging.info(data)
         self.finalfix()
-
+        
     def check(self):
-        except_value = True
-        result = bsf.command_search(self.config['change']['set'])
-        if len(result[0]) != 0:
-            result2 = bsf.search_audit_rule(self.config['query']['path'][0])
-            if result2[1] == 0:
-                except_value = True
+        except_value=True
+        result=bsf.command_search(self.config['change']['set'])
+        if len(result[0])!=0:
+            result2=bsf.search_audit_rule(self.config['query']['path'][0])
+            if result2[1]==0:
+                except_value=True
             else:
-                except_value = False
+                except_value=False
         else:
-            result3 = bsf.pipe_grep_shell(self.config['query']['form'], self.config['query']['path'][0], self.config['change']['value'])
-            if result3[1] == 0:
-                except_value = True
+            result3=bsf.pipe_grep_shell(self.config['query']['form'],self.config['query']['path'][0],self.config['change']['value'])
+            if result3[1]==0:
+                except_value=True
             else:
-                except_value = False
+                except_value=False
         return except_value
 
     def rollback(self):
@@ -69,18 +71,20 @@ class AuditEtc_4(base_fix):
         bsf.delete_audit_rule()
         bsf.reload_audit_rules()
         result = self.check()
+        # 每次还原前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
         if result == False:
-            self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 0
+            self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 0
             self.status_form.to_pickle(self.pkl_file)
+            
 
     def reset(self):
         self.rollback()
         self.fix()
 
     def get_des(self):
-        description = self.config['description']
+        description=self.config['description']
         return description
