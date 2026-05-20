9f215d1e39ec352a24193e202bd77d70eff2e89b
caojingbo <caojb22@chinaunicom.cn>
2026-05-20 19:22:33 +0800

    Complete CheckPeDoSock_18 implementation

--- department_2_policy/CheckPeDoSock_18/CheckPeDoSock_18.py
+++ department_2_policy/CheckPeDoSock_18/CheckPeDoSock_18.py
@@ -11,64 +11,68 @@ from base_fix import base_fix
 from base_shell_function import base_shell_function as bsf
 from base_shell import base_shell
 import logging
+# import pandas as pd
 import Panda as pd
 logging.getLogger(__name__)
-
-class CheckPeDoSock_18(base_fix):
-
+#TestCase-部门编号-子加固项名称-子加固项编号
+class CheckPeDoSock_18(base_fix):    
     def __init__(self):
         super().__init__()
+
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.config_file = os.path.join(self.current_dir, 'CheckPeDoSock_18.yaml')
-        with open(file=self.config_file, mode='r+', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
-        self.pkl_file = os.path.join(os.path.dirname(self.current_dir), 'data_status.pkl')
-        self.config = config
-        self.status = None
+        self.config_file = os.path.join(self.current_dir, "CheckPeDoSock_18.yaml")
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
+        self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
+        self.config=config
+        self.status=None
 
     def finalfix(self):
-        self.status = 2
-        self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
         self.status = 1
+        # 每次加固前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
-        bsf.chmod_file(self.config['change']['value'], self.config['query']['path'])
-        result = self.check()
-        if result == True:
-            self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 2
-        data = 'type:fix,des:{}'.format(self.config['description'])
+        bsf.chmod_file(self.config['change']['value'],self.config['query']['path'])
+        result=self.check()
+        if result==True:
+            self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
+        data='type:fix,des:{}'.format(self.config['description'])
         logging.info(data)
         self.finalfix()
-
+        
     def check(self):
-        except_value = True
-        result = bsf.file_permission(self.config['query']['path'])
-        if result[1] == 0:
-            output = result[0].strip()
-            if output != self.config['change']['value']:
-                except_value = False
+        except_value=True
+        result=bsf.file_permission(self.config['query']['path'])
+        if result[1]==0:
+            output=result[0].strip()
+            if output!=self.config['change']['value']:
+                except_value=False
         return except_value
 
     def rollback(self):
-        bsf.chmod_file(self.config['change']['value'], self.config['query']['path'])
-        result = self.check()
+        bsf.chmod_file(self.config['change']['value'],self.config['query']['path'])
+        result=self.check()
+        # 每次还原前都读取最新的 pkl，避免覆盖其他加固项状态
         if os.path.exists(self.pkl_file):
             self.status_form = pd.read_pickle(self.pkl_file)
         else:
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
         if result == False:
-            self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 0
+            self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 0
             self.status_form.to_pickle(self.pkl_file)
+            
 
     def reset(self):
         self.rollback()
         self.fix()
 
     def get_des(self):
-        description = self.config['description']
+        description=self.config['description']
         return description
