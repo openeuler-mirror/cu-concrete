f45ce5acbd087e1f894a104f9083c33eac29ca25
caojingbo <caojb22@chinaunicom.cn>
2026-05-21 15:22:21 +0800

    Adjust DelDangeFile_19 get des

--- department_1_policy/DelDangeFile_19/DelDangeFile_19.py
+++ department_1_policy/DelDangeFile_19/DelDangeFile_19.py
@@ -89,9 +89,18 @@ class DelDangeFile_19(base_fix):
                     original_file = backup_file[:-4]
                     bsf.cp_shell(backup_file, original_file)
         result = self.check()
+        if os.path.exists(self.pkl_file):
+            self.status_form = pd.read_pickle(self.pkl_file)
+        else:
+            self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
+        if result == False:
+            self.status_form.loc[str(self.config['dep']) + str(self.config['id']), 'status'] = 0
+            self.status_form.to_pickle(self.pkl_file)
 
     def reset(self):
-        pass
+        self.rollback()
+        self.fix()
 
     def get_des(self):
-        pass
+        description = self.config['description']
+        return description
