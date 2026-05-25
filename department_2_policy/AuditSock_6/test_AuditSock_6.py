8e57104447a87ff7e76e336d2d360a2c497b0eef
caojingbo <caojb22@chinaunicom.cn>
2026-05-25 15:45:38 +0800

    Implement test_AuditSock_6 build instance

--- department_2_policy/AuditSock_6/test_AuditSock_6.py
+++ department_2_policy/AuditSock_6/test_AuditSock_6.py
@@ -33,9 +33,16 @@ def prepare_files():
     df = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
     df.to_pickle(pkl_path)
     yield
+    for fp in [pkl_path, '/tmp/AuditSock_6.yaml', file_sock, rule_file, auditctl, backup_path]:
+        if os.path.exists(fp):
+            os.remove(fp)
 
 def build_instance():
-    pass
+    mod = load_module()
+    cls = getattr(mod, 'AuditSock_6')
+    obj = cls()
+    obj.config_file = '/tmp/AuditSock_6.yaml'
+    obj.pkl_file = pkl_path
 
 def test_init():
     pass
