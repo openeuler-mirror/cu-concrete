ce4d2b2b7821f9660ae71ae86b3d0267b5acc1f0
caojingbo <caojb22@chinaunicom.cn>
2026-05-25 18:46:16 +0800

    Implement test_CheckPeSysconfig_20 implement instance

--- department_2_policy/CheckPeSysconfig_20/test_CheckPeSysconfig_20.py
+++ department_2_policy/CheckPeSysconfig_20/test_CheckPeSysconfig_20.py
@@ -38,6 +38,21 @@ def build_instance():
     mod = load_module()
     cls = getattr(mod, 'CheckPeSysconfig_20')
     obj = cls()
+    obj.config_file = '/tmp/CheckPeSysconfig_20.yaml'
+    obj.pkl_file = pkl_path
+    obj.current_dir = '/tmp'
+    if os.path.exists(yaml_path):
+        with open(yaml_path, 'r', encoding='utf-8') as f:
+            yaml_cfg = yaml.load(f, Loader=yaml.Loader)
+        obj.config = yaml_cfg
+        if 'query' in obj.config and 'path' in obj.config['query']:
+            obj.config['query']['path'] = file_path
+        obj.config['backup_path'] = backup_path
+    else:
+        obj.config = {'dep': 2, 'id': 20, 'query': {'path': file_path}, 'change': {'value': '644'}, 'backup_path': backup_path, 'description': '确保 sysconfig 相关文件权限为 644'}
+    obj.config['change'] = obj.config.get('change')
+    obj.status_form = pd.read_pickle(pkl_path)
+    return (mod, obj)
 
 def test_init():
     pass
