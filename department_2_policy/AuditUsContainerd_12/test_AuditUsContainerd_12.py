7ed2806e6f0890311207b047589996d384c336bc
caojingbo <caojb22@chinaunicom.cn>
2026-05-25 16:28:43 +0800

    Implement test_AuditUsContainerd_12 build instance

--- department_2_policy/AuditUsContainerd_12/test_AuditUsContainerd_12.py
+++ department_2_policy/AuditUsContainerd_12/test_AuditUsContainerd_12.py
@@ -39,7 +39,11 @@ def prepare_files():
             os.remove(fp)
 
 def build_instance():
-    pass
+    mod = load_module()
+    cls = getattr(mod, 'AuditUsContainerd_12')
+    obj = cls()
+    obj.config_file = '/tmp/AuditUsContainerd_12.yaml'
+    obj.pkl_file = pkl_path
 
 def test_init():
     pass
