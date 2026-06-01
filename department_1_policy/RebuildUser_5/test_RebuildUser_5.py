6df12e48d9114ecc661b6ac3b6e63b6feb68415e
caojingbo <caojb22@chinaunicom.cn>
2026-06-01 19:09:32 +0800

    Extend test_RebuildUser_5 test fix

--- department_1_policy/RebuildUser_5/test_RebuildUser_5.py
+++ department_1_policy/RebuildUser_5/test_RebuildUser_5.py
@@ -38,10 +38,13 @@ def test_init():
     assert isinstance(obj.status_form, pd.DataFrame)
 
 def test_finalfix():
-    pass
+    obj = build_instance()
+    obj.finalfix()
+    status_df = pd.read_pickle(pkl_path)
+    assert status_df.loc['15', 'status'] == 2
 
 def test_fix():
-    pass
+    obj = build_instance()
 
 def test_check():
     pass
