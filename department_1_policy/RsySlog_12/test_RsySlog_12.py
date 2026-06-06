70b4798b7189d6104b967f0bbe105074f28f1ebe
caojingbo <caojb22@chinaunicom.cn>
2026-06-06 15:33:11 +0800

    Adjust test_RsySlog_12 test get des

--- department_1_policy/RsySlog_12/test_RsySlog_12.py
+++ department_1_policy/RsySlog_12/test_RsySlog_12.py
@@ -63,6 +63,11 @@ def test_rollback():
 def test_reset():
     obj = build_instance()
     obj.reset()
+    status_df = pd.read_pickle(pkl_path)
+    val = status_df.loc['112', 'status']
+    assert val == 2
 
 def test_get_des():
-    pass
+    obj = build_instance()
+    des = obj.get_des()
+    assert des == '远程系统日志设定'
