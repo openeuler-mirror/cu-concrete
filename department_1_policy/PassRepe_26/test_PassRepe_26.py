4afb7cf7a2251b52ee8e06a2ac44dc99757ece50
caojingbo <caojb22@chinaunicom.cn>
2026-06-06 14:47:51 +0800

    Optimize test_PassRepe_26 test get des

--- department_1_policy/PassRepe_26/test_PassRepe_26.py
+++ department_1_policy/PassRepe_26/test_PassRepe_26.py
@@ -65,6 +65,11 @@ def test_rollback():
 def test_reset():
     obj = build_instance()
     obj.reset()
+    status_df = pd.read_pickle(pkl_path)
+    val = status_df.loc['126', 'status']
+    assert val == 2
 
 def test_get_des():
-    pass
+    obj = build_instance()
+    des = obj.get_des()
+    assert des == '密码不能与历史的5个重复密码相同'
