59eb860930712f8347edab8765be5d78a3562ecf
caojingbo <caojb22@chinaunicom.cn>
2026-06-09 15:21:53 +0800

    Extend test_AuditRunc_14 test check command search branch not present

--- department_2_policy/AuditRunc_14/test_AuditRunc_14.py
+++ department_2_policy/AuditRunc_14/test_AuditRunc_14.py
@@ -110,16 +110,28 @@ def test_check_command_search_branch(monkeypatch):
 
         @staticmethod
         def command_search(arg):
+            return (form,)
+
+        @staticmethod
+        def search_audit_rule(path):
+            return (None, 0)
+    monkeypatch.setattr(mod, 'bsf', FakeBSF)
+    assert obj.check() is True
+
+def test_check_command_search_branch_not_present(monkeypatch):
+    mod, obj = build_instance()
+    form = obj.config['query']['form']
+
+    class FakeBSF:
+
+        @staticmethod
+        def command_search(arg):
             pass
 
         @staticmethod
         def search_audit_rule(path):
             pass
     monkeypatch.setattr(mod, 'bsf', FakeBSF)
-    assert obj.check() is True
-
-def test_check_command_search_branch_not_present(monkeypatch):
-    pass
 
 def test_check_pipe_grep_branch(monkeypatch):
     pass
