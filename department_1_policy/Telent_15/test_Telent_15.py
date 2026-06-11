e6d3238d8dc2952c44fba550da267170b0ce0bbb
caojingbo <caojb22@chinaunicom.cn>
2026-06-11 10:35:02 +0800

    Complete test_Telent_15 implementation

--- department_1_policy/Telent_15/test_Telent_15.py
+++ department_1_policy/Telent_15/test_Telent_15.py
@@ -2,16 +2,20 @@ import os
 import pytest
 import pandas as pd
 from Telent_15 import Telent_15
+
 yaml_path = os.path.join(os.path.dirname(__file__), 'Telent_15.yaml')
 pkl_path = '/tmp/test_data_status.pkl'
 telnet_conf_path = '/tmp/test_telnet_conf'
 
 @pytest.fixture(autouse=True)
 def prepare_files():
+    # 复制 yaml
     if os.path.exists(yaml_path):
         os.system(f'cp {yaml_path} /tmp/Telent_15.yaml')
+    # 构造 telnet 配置文件
     with open(telnet_conf_path, 'w') as f:
         f.write('disable = yes\n')
+    # 构造 data_status.pkl
     df = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
     df.to_pickle(pkl_path)
     yield
@@ -24,7 +28,25 @@ def build_instance():
     obj.config_file = '/tmp/Telent_15.yaml'
     obj.pkl_file = pkl_path
     obj.current_dir = '/tmp'
-    obj.config = {'dep': 1, 'id': 15, 'query': {'form': '^disable', 'path': telnet_conf_path}, 'change': {'value': ['disable = yes', 'openssh*', 'systemctl disable  telnet.socket', 'systemctl stop telnet.socket', 'systemctl enable telnet.socket', 'systemctl start telnet.socket']}, 'description': '关闭telent,使用openssh'}
+    obj.config = {
+        'dep': 1,
+        'id': 15,
+        'query': {
+            'form': '^disable',
+            'path': telnet_conf_path
+        },
+        'change': {
+            'value': [
+                'disable = yes',
+                'openssh*',
+                'systemctl disable  telnet.socket',
+                'systemctl stop telnet.socket',
+                'systemctl enable telnet.socket',
+                'systemctl start telnet.socket'
+            ]
+        },
+        'description': '关闭telent,使用openssh'
+    }
     obj.status_form = pd.read_pickle(pkl_path)
     return obj
 
