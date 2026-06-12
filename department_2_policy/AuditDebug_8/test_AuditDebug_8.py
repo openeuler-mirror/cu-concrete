ad3e61f7fee1c3f21fddc051936505fbcc07e2ce
caojingbo <caojb22@chinaunicom.cn>
2026-06-12 15:16:17 +0800

    Flesh out pipe grep assertions

--- department_2_policy/AuditDebug_8/test_AuditDebug_8.py
+++ department_2_policy/AuditDebug_8/test_AuditDebug_8.py
@@ -139,7 +139,19 @@ def test_check_pipe_grep_branch(monkeypatch):
     assert obj.check() is True
 
 def test_check_pipe_grep_branch_not_found(monkeypatch):
-    pass
+    mod, obj = build_instance()
+
+    class FakeBSF:
+
+        @staticmethod
+        def command_search(arg):
+            return ('',)
+
+        @staticmethod
+        def pipe_grep_shell(form_arg, path, value):
+            return ('', 2)
+    monkeypatch.setattr(mod, 'bsf', FakeBSF)
+    assert obj.check() is False
 
 def test_rollback_updates_status_when_check_fails(monkeypatch):
     pass
