2fe52b383faa5c296ca43ba3086c195b5702ac01
caojingbo <caojb22@chinaunicom.cn>
2026-06-17 12:29:37 +0800

    Refine test_AuditSysconfig_11 test reset

--- department_2_policy/AuditSysconfig_11/test_AuditSysconfig_11.py
+++ department_2_policy/AuditSysconfig_11/test_AuditSysconfig_11.py
@@ -170,19 +170,19 @@ def test_rollback_updates_status_when_check_fails(monkeypatch):
 
         @staticmethod
         def delete_audit_rule():
-            pass
+            return None
 
         @staticmethod
         def reload_audit_rules():
-            pass
+            return None
 
         @staticmethod
         def command_search(arg):
-            pass
+            return ('',)
 
         @staticmethod
         def pipe_grep_shell(form_arg, path, value):
-            pass
+            return ('', 1)
     monkeypatch.setattr(mod, 'bsf', FakeBSF)
     obj.status_form.loc['211', 'status'] = 1
     obj.status_form.to_pickle(pkl_path)
@@ -191,7 +191,8 @@ def test_rollback_updates_status_when_check_fails(monkeypatch):
     assert status_df.loc['211', 'status'] == 0
 
 def test_reset(monkeypatch):
-    pass
+    mod, obj = build_instance()
+    monkeypatch.setattr(mod.bsf, 'reload_audit_rules', lambda *a, **k: None)
 
 def test_get_des():
     pass
