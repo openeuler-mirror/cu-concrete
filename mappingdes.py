45825fe22dcc7ca3b5bc45c6b96c095f5fea8a1c
caojingbo <caojb22@chinaunicom.cn>
2026-06-22 20:03:42 +0800

    Implement mappingdes load check class (mappingdes 5)

--- mappingdes.py
+++ mappingdes.py
@@ -27,6 +27,42 @@ def load_check_class(department_id):
     process = subprocess.Popen(cmd, stdin=subprocess.PIPE, text=True)
     listfolder = [folder for folder in os.listdir(department_path) if folder not in ['.git', '__pycache__'] and os.path.isdir(os.path.join(department_path, folder))]
     status = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
+    status.to_pickle(department_data_pkl)
+    for i, folder in enumerate(listfolder):
+        folder_path = os.path.join(department_path, folder)
+        module_name = folder
+        module_path = os.path.join(folder_path, f'{module_name}.py')
+        if not os.path.isfile(module_path):
+            continue
+        if module_name not in _cls_cache:
+            if module_name not in _module_cache:
+                spec = importlib.util.spec_from_file_location(module_name, module_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                _module_cache[module_name] = module
+            cls = getattr(_module_cache[module_name], module_name)
+            _cls_cache[module_name] = cls
+        else:
+            cls = _cls_cache[module_name]
+        try:
+            instance = cls()
+            if not instance.check():
+                fixinstance[instance.get_des()] = instance
+                status.loc[str(instance.config['dep']) + str(instance.config['id']), 'status'] = 0
+                status.loc[str(instance.config['dep']) + str(instance.config['id']), 'module_name'] = module_name
+                status.loc[str(instance.config['dep']) + str(instance.config['id']), 'module_path'] = module_path
+            else:
+                rbinstancee[instance.get_des()] = instance
+                status.loc[str(instance.config['dep']) + str(instance.config['id']), 'status'] = 2
+                status.loc[str(instance.config['dep']) + str(instance.config['id']), 'module_name'] = module_name
+                status.loc[str(instance.config['dep']) + str(instance.config['id']), 'module_path'] = module_path
+            percent = int((i + 1) / len(listfolder) * 100)
+            process.stdin.write(f'{percent}\n')
+            process.stdin.flush()
+            time.sleep(0.001)
+        except AttributeError:
+            raise AttributeError(f'模块 {module_name} 中未找到类 {module_name}')
+    status.to_pickle(department_data_pkl)
 
 def load_sec_class(department_id):
     """
