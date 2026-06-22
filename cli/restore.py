c4bdc785cfd245b8f74e65ff3e51abd0cd2a438f
caojingbo <caojb22@chinaunicom.cn>
2026-06-22 11:46:44 +0800

    Set up input validation for restore-all command

--- cli/restore.py
+++ cli/restore.py
@@ -52,6 +52,29 @@ class Restorer:
         print('正在扫描系统安全状态...')
         path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
         flag_path = os.path.join(path, 'flag.txt')
+        if not os.path.exists(flag_path):
+            with open(flag_path, 'w', encoding='utf-8') as f:
+                content = f.write('1')
+                flag = 1
+        else:
+            with open(flag_path, 'r', encoding='utf-8') as f:
+                flag = int(f.read().strip())
+        if flag == 1:
+            instance_tuple = self.checker.sub_checklist_noui()
+            available_items = instance_tuple[1] if instance_tuple else []
+            flag += 1
+            with open(flag_path, 'w', encoding='utf-8') as f:
+                f.write(str(flag))
+        else:
+            instance_tuple = self.checker.sec_checklist()
+            available_items = instance_tuple[1] if instance_tuple else []
+        if not available_items:
+            print('没有可还原的项目')
+            self.logger.info('没有可还原的项目')
+            return
+        print(f'发现 {len(available_items[0])} 个可还原项，开始执行...')
+        self._execute_restore(available_items)
+        self.logger.info('全部还原项执行完成')
 
     def restore_items(self, item_ids):
         """还原指定ID的项"""
