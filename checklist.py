593cfe6b2cf1e91cf3a27529002268759c85604b
caojingbo <caojb22@chinaunicom.cn>
2026-06-22 16:20:02 +0800

    Sketch out checklist module with empty functions and docstrings

---
+++ checklist.py
@@ -0,0 +1,27 @@
+from whiptail import Whiptail
+import time
+import subprocess
+import os
+from mappingdes import load_check_class, load_sec_class, load_departments, load_departments_no_ui
+import re
+import logging
+TITLE = '安全加固工具'
+HEIGHT = 25
+WIDTH = 60
+
+class checklist:
+
+    def get_folder(self, i):
+        folder_str = 'department_{}_policy'.format(i)
+        return folder_str
+
+    def sub_checklist(self):
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        fixinstanse = []
+        fix_instance = {}
+
+    def sec_checklist(self):
+        pass
+
+    def sub_checklist_noui(self):
+        pass
