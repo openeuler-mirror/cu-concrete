b62a447f5661e65969607f9917d0552bcb746180
caojingbo <caojb22@chinaunicom.cn>
2026-06-22 12:26:07 +0800

    Refine harden items (harden 2)

--- cli/harden.py
+++ cli/harden.py
@@ -85,6 +85,18 @@ class Hardener:
         instance_tuple = self.checker.sec_checklist()
         available_items = instance_tuple[0] if instance_tuple else []
         target_item_ids = item_ids[1].split(',') if len(item_ids) > 1 else []
+        target_items_list = [item for item in available_items[0] if item[0] in target_item_ids]
+        target_instances = {}
+        for description, instance in available_items[1].items():
+            dep_id = '{}_{}'.format(instance.config['dep'], instance.config['id'])
+            if dep_id in target_item_ids:
+                target_instances[description] = instance
+        target_items = (target_items_list, target_instances)
+        if not target_items_list:
+            print('没有匹配的可加固项')
+            self.logger.warning('没有匹配的可加固项')
+            return
+        print(f'找到 {len(target_items[0])} 个匹配项，开始执行...')
 
     def _execute_harden(self, items):
         """执行加固操作"""
