4ddf413b0a2d4bf401f3cab28abb4d05be1722fc
caojingbo <caojb22@chinaunicom.cn>
2026-07-11 17:30:09 +0800

    feat: finalize policy module for addsecure

--- department_1_policy/AddSecure_13/AddSecure_13.py
+++ department_1_policy/AddSecure_13/AddSecure_13.py
@@ -13,24 +13,22 @@ from base_shell import base_shell
 import logging
 # import pandas as pd
 import Panda as pd
-logger = logging.getLogger(__name__)
-# TestCase-部门编号-子加固项名称-子加固项编号
-# 优化：统一日志变量命名
+logging.getLogger(__name__)
+#TestCase-部门编号-子加固项名称-子加固项编号
 class AddSecure_13(base_fix):    
     def __init__(self):
         super().__init__()
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "AddSecure_13.yaml")
-        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
 
     def finalfix(self):
-        self.status = 2
-        key = str(self.config['dep']) + str(self.config['id'])
-        self.status_form.loc[key, 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
@@ -84,18 +82,17 @@ class AddSecure_13(base_fix):
             u_g="{}:{}".format(self.config['query']['form'],self.config['query']['form'])
             cmd8=['chown',u_g,self.config['change']['path4']]
             base_shell(cmd8)
-        data = f"type:fix,des:{self.config['description']}"
+        data='type:fix,des:{}'.format(self.config['description'])
         logging.info(data) 
         self.finalfix()  
         
     def check(self):
-        """检查策略是否满足要求。"""
-        expected_value = True
+        except_value=True
         cmd=['id',self.config['query']['form']]
         result=base_shell(cmd)
         if result[1]!=0:
-            expected_value = False
-        return expected_value
+            except_value=False
+        return except_value
         
     def rollback(self):
         result=base_shell(['lsattr','/etc/passwd'])
