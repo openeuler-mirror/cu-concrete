e9be256a90709ce2b11a8084655125f6f61e80b1
caojingbo <caojb22@chinaunicom.cn>
2026-07-13 17:27:12 +0800

    fix: rename variables and status key for disableftpanonymous

--- department_1_policy/DisableFtpAnonymous_21/DisableFtpAnonymous_21.py
+++ department_1_policy/DisableFtpAnonymous_21/DisableFtpAnonymous_21.py
@@ -21,8 +21,8 @@ class DisableFtpAnonymous_21(base_fix):
         super().__init__()
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "DisableFtpAnonymous_21.yaml")
-        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
-            config = yaml.load(f,Loader = yaml.Loader)
+        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
+            config = yaml.load(f, Loader=yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
@@ -56,6 +56,7 @@ class DisableFtpAnonymous_21(base_fix):
         self.finalfix() 
 
     def check(self):
+        """检查策略是否满足要求。"""
         expected_value = True
         result2=bsf.grep_shell(self.config['change']['value'][0],self.config['query']['path'][1])
         result3=bsf.grep_shell(self.config['change']['value'][0],self.config['query']['path'][2])
