4e43431afe063905d83fcaf6739b055519d66061
caojingbo <caojb22@chinaunicom.cn>
2026-07-14 16:26:39 +0800

    feat: finalize policy module for resetlsrm

--- department_1_policy/Resetlsrm_27/Resetlsrm_27.py
+++ department_1_policy/Resetlsrm_27/Resetlsrm_27.py
@@ -13,24 +13,22 @@ from base_shell import base_shell
 import logging
 # import pandas as pd
 import Panda as pd
-logger = logging.getLogger(__name__)
-# TestCase-部门编号-子加固项名称-子加固项编号
-# 优化：统一日志变量命名
+logging.getLogger(__name__)
+#TestCase-部门编号-子加固项名称-子加固项编号
 class Resetlsrm_27(base_fix):    
     def __init__(self):
         super().__init__()
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "Resetlsrm_27.yaml")
-        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
 
     def finalfix(self):
-        self.status = 2
-        key = str(self.config['dep']) + str(self.config['id'])
-        self.status_form.loc[key, 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
@@ -60,20 +58,19 @@ class Resetlsrm_27(base_fix):
             value=self.config['change']['value'][1]
             base_shell(cmd,input=f'\n{value}')
 
-        data = f"type:fix,des:{self.config['description']}"
+        data='type:fix,des:{}'.format(self.config['description'])
         logging.info(data)
         self.finalfix()
     
     def check(self):
-        """检查策略是否满足要求。"""
-        expected_value = True
+        except_value=True
         result=bsf.grep_shell(self.config['change']['value'][0],self.config['query']['path'])
         if len(result[0])==0:
-            expected_value = False
+            except_value=False
         result=bsf.grep_shell(self.config['change']['value'][1],self.config['query']['path'])
         if len(result[0])==0:
-            expected_value = False
-        return expected_value
+            except_value=False
+        return except_value
     
     def rollback(self):
         # 每次还原前都读取最新的 pkl，避免覆盖其他加固项状态
