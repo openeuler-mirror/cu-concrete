54021e52142783d2d9788a91ab14cd897414b984
caojingbo <caojb22@chinaunicom.cn>
2026-07-14 09:29:35 +0800

    feat: finalize policy module for histsize

--- department_1_policy/HistSize_23/HistSize_23.py
+++ department_1_policy/HistSize_23/HistSize_23.py
@@ -13,23 +13,21 @@ from base_shell import base_shell
 import logging
 # import pandas as pd
 import Panda as pd
-# TestCase-部门编号-子加固项名称-子加固项编号
-# 优化：统一日志变量命名
+#TestCase-部门编号-子加固项名称-子加固项编号
 class HistSize_23(base_fix):    
     def __init__(self):
         super().__init__()
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "HistSize_23.yaml")
-        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
-            config = yaml.load(f, Loader=yaml.Loader)
+        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
+            config = yaml.load(f,Loader = yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
 
     def finalfix(self):
-        self.status = 2
-        key = str(self.config['dep']) + str(self.config['id'])
-        self.status_form.loc[key, 'status'] = 2
+        self.status=2
+        self.status_form.loc[str(self.config['dep'])+str(self.config['id']),'status']=2
         self.status_form.to_pickle(self.pkl_file)
 
     def fix(self):
@@ -55,17 +53,16 @@ class HistSize_23(base_fix):
             strs=f'source {self.config["query"]["path"]}'
             cmd=['bash','-c',strs]
             base_shell(cmd)
-        data = f"type:fix,des:{self.config['description']}"
+        data='type:fix,des:{}'.format(self.config['description'])
         logging.info(data) 
         self.finalfix()
     
     def check(self):
-        """检查策略是否满足要求。"""
-       expected_value = True
+       except_value=True
        result=bsf.grep_shell(self.config['query']['form'],self.config['query']['path'])
        if result[0]!=self.config['change']['value'] or len(result[0])==0:
-           expected_value = False
-       return expected_value
+           except_value=False
+       return except_value
     
     def rollback(self):
         bsf.remove_line(self.config['change']['value'],self.config['query']['path'])
