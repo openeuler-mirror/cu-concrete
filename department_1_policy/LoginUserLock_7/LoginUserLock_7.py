b6a3e0ca7139801d2d254cebddbf020a327b033b
caojingbo <caojb22@chinaunicom.cn>
2026-07-14 11:53:26 +0800

    fix: py logging and readme touch for loginuserlock

--- department_1_policy/LoginUserLock_7/LoginUserLock_7.py
+++ department_1_policy/LoginUserLock_7/LoginUserLock_7.py
@@ -13,8 +13,9 @@ from base_shell import base_shell
 import logging
 # import pandas as pd
 import Panda as pd
-logging.getLogger(__name__)
-#TestCase-部门编号-子加固项名称-子加固项编号
+logger = logging.getLogger(__name__)
+# TestCase-部门编号-子加固项名称-子加固项编号
+# 优化：统一日志变量命名
 class LoginUserLock_7(base_fix):    
     def __init__(self):
         super().__init__()
