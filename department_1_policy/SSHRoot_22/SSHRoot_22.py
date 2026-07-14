c6e5245d38ef4216c4e14337a8e5f1f73c72cca1
caojingbo <caojb22@chinaunicom.cn>
2026-07-14 18:07:07 +0800

    fix: py logging and readme touch for sshroot

--- department_1_policy/SSHRoot_22/SSHRoot_22.py
+++ department_1_policy/SSHRoot_22/SSHRoot_22.py
@@ -13,8 +13,9 @@ from base_shell import base_shell
 import logging
 # import pandas as pd
 import Panda as pd
-logging.getLogger(__name__)
-#TestCase-部门编号-子加固项名称-子加固项编号
+logger = logging.getLogger(__name__)
+# TestCase-部门编号-子加固项名称-子加固项编号
+# 优化：统一日志变量命名
 class SSHRoot_22(base_fix):    
     def __init__(self):
         super().__init__()
@@ -50,7 +51,7 @@ class SSHRoot_22(base_fix):
         base_shell(cmd2)
         cmd3=['systemctl','restart','sshd']
         base_shell(cmd3)
-        data='type:fix,des:{}'.format(self.config['description'])
+        data = f"type:fix,des:{self.config['description']}"
         logging.info(data) 
         self.finalfix()
     
