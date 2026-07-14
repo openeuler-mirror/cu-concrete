2edc8426476b424c7eb368e489b2111420dc0022
caojingbo <caojb22@chinaunicom.cn>
2026-07-14 18:29:28 +0800

    fix: rename variables and status key for sshroot

--- department_1_policy/SSHRoot_22/SSHRoot_22.py
+++ department_1_policy/SSHRoot_22/SSHRoot_22.py
@@ -21,8 +21,8 @@ class SSHRoot_22(base_fix):
         super().__init__()
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "SSHRoot_22.yaml")
-        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
-            config = yaml.load(f,Loader = yaml.Loader)
+        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
+            config = yaml.load(f, Loader=yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
@@ -56,6 +56,7 @@ class SSHRoot_22(base_fix):
         self.finalfix()
     
     def check(self):
+        """检查策略是否满足要求。"""
         expected_value = True
         result1=bsf.grep_shell(self.config['change']['value'][0],self.config['query']['path'][0])
         if result1[0]!=self.config['change']['value'][0]:
