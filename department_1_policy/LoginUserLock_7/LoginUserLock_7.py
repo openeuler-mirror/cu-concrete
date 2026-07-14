a5961b49eb107cb65ed283926382d7a38efbc7ac
caojingbo <caojb22@chinaunicom.cn>
2026-07-14 12:16:56 +0800

    fix: rename variables and status key for loginuserlock

--- department_1_policy/LoginUserLock_7/LoginUserLock_7.py
+++ department_1_policy/LoginUserLock_7/LoginUserLock_7.py
@@ -22,8 +22,8 @@ class LoginUserLock_7(base_fix):
 
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "LoginUserLock_7.yaml")
-        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
-            config = yaml.load(f,Loader = yaml.Loader)
+        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
+            config = yaml.load(f, Loader=yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
@@ -52,6 +52,7 @@ class LoginUserLock_7(base_fix):
         self.finalfix()
 
     def check(self):
+        """检查策略是否满足要求。"""
         expected_value = True
         result=bsf.grep_shell(self.config['change']['value'][1],self.config['query']['path'])
         if result[0]!=self.config['change']['value'][1]:
