c7b30dd5b7952d19c6e241511e566ed3a4e82478
caojingbo <caojb22@chinaunicom.cn>
2026-07-17 19:32:26 +0800

    fix: rename variables and status key for checkowdebug

--- department_2_policy/CheckOwDebug_24/CheckOwDebug_24.py
+++ department_2_policy/CheckOwDebug_24/CheckOwDebug_24.py
@@ -22,8 +22,8 @@ class CheckOwDebug_24(base_fix):
 
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "CheckOwDebug_24.yaml")
-        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
-            config = yaml.load(f,Loader = yaml.Loader)
+        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
+            config = yaml.load(f, Loader=yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
@@ -49,6 +49,7 @@ class CheckOwDebug_24(base_fix):
         self.finalfix()
         
     def check(self):
+        """检查策略是否满足要求。"""
         expected_value = True
         result=bsf.file_owner(self.config['query']['path'])
         if result[1]==0:
