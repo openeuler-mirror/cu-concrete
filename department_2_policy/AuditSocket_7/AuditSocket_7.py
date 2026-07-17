42d74997e84f5a1279454b7e90276ee26edae745
caojingbo <caojb22@chinaunicom.cn>
2026-07-17 15:44:56 +0800

    fix: rename variables and status key for auditsocket

--- department_2_policy/AuditSocket_7/AuditSocket_7.py
+++ department_2_policy/AuditSocket_7/AuditSocket_7.py
@@ -22,8 +22,8 @@ class AuditSocket_7(base_fix):
 
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
         self.config_file = os.path.join(self.current_dir, "AuditSocket_7.yaml")
-        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
-            config = yaml.load(f,Loader = yaml.Loader)
+        with open(file=self.config_file, mode='r', encoding='utf-8') as f:
+            config = yaml.load(f, Loader=yaml.Loader)
         self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
         self.config=config
         self.status=None
@@ -52,6 +52,7 @@ class AuditSocket_7(base_fix):
         self.finalfix()
         
     def check(self):
+        """检查策略是否满足要求。"""
         expected_value = True
         result=bsf.command_search(self.config['change']['set'])
         if len(result[0])!=0:
