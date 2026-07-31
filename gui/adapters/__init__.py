6713cf9e4f4ac33da594861d7926ae6f3ba31bce
caojingbo <caojb22@chinaunicom.cn>
2026-07-31 17:18:49 +0800

    feat: add adapters init py

---
+++ gui/adapters/__init__.py
@@ -0,0 +1,4 @@
+from .base_adapter import IPolicyAdapter, ActionResult
+from .policy_adapter import PolicyAdapter
+from .department_adapter import DepartmentAdapter
+from .api_client_adapter import ApiClientAdapter
