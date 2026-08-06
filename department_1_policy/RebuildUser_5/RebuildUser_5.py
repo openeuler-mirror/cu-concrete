a0bbe616d32b7a15666e2bb5cf7f277e5c40c8ec
caojingbo <caojb22@chinaunicom.cn>
2026-08-06 22:19:36 +0800

    feat: update rebuilduser 5 init and imports

--- department_1_policy/RebuildUser_5/RebuildUser_5.py
+++ department_1_policy/RebuildUser_5/RebuildUser_5.py
@@ -4,14 +4,11 @@ parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
 project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(project_dir)
-import json
 import yaml
-import time
 from base_fix import base_fix
 from base_shell_function import base_shell_function as bsf
 from base_shell import base_shell
 import logging
-# import pandas as pd
 import Panda as pd
 logging.getLogger(__name__)
 #TestCase-部门编号-子加固项名称-子加固项编号
@@ -20,12 +17,12 @@ class RebuildUser_5(base_fix):
         super().__init__()
 
         self.current_dir = os.path.dirname(os.path.abspath(__file__))
-        self.config_file = os.path.join(self.current_dir, "RebuildUser_5.yaml")
-        with open(file=self.config_file,mode='r+',encoding='utf-8') as f :
-            config = yaml.load(f,Loader = yaml.Loader)
-        self.pkl_file=os.path.join(os.path.dirname(self.current_dir),'data_status.pkl')
-        self.config=config
-        self.status=None
+        self.config_file_path = os.path.join(self.current_dir, "RebuildUser_5.yaml")
+        with open(file = self.config_file_path, mode='r+', encoding='utf-8') as f :
+            config = yaml.load(f, Loader = yaml.Loader)
+        self.pkl_file = os.path.join(os.path.dirname(self.current_dir), 'data_status.pkl')
+        self.config = config
+        self.status = None
 
     def finalfix(self):
         self.status=2
@@ -41,22 +38,35 @@ class RebuildUser_5(base_fix):
             self.status_form = pd.DataFrame(columns=['status', 'module_name', 'module_path'])
         self.status_form.loc[str(self.config['dep'])+str(self.config['id']), 'status'] = 1
         self.status_form.to_pickle(self.pkl_file)
-        if len(bsf.grep_shell(self.config['query']['form'][0],self.config['query']['path'])[0])!=0:
+        
+        # PASS_MAX_DAYS
+        PASS_MAX_DAYS_RESULT = bsf.grep_shell(self.config['query']['form'][0],self.config['query']['path'])[0]
+        if len(PASS_MAX_DAYS_RESULT)!=0:
             bsf.sed_shell(bsf.grep_shell(self.config['query']['form'][0],self.config['query']['path'])[0],self.config['change']['value'][0],self.config['query']['path']) 
         else:
             bsf.append_line(self.config['change']['value'][0],self.config['query']['path'])
-        if len(bsf.grep_shell(self.config['query']['form'][1],self.config['query']['path'])[0])!=0:
+        
+        # PASS_MIN_DAYS
+        PASS_MIN_DAYS_RESULT = bsf.grep_shell(self.config['query']['form'][1],self.config['query']['path'])[0]
+        if len(PASS_MIN_DAYS_RESULT)!=0:
             bsf.sed_shell(bsf.grep_shell(self.config['query']['form'][1],self.config['query']['path'])[0],self.config['change']['value'][1],self.config['query']['path']) 
         else:
             bsf.append_line(self.config['change']['value'][1],self.config['query']['path'])
-        if len(bsf.grep_shell(self.config['query']['form'][2],self.config['query']['path'])[0])!=0:
+        
+        # PASS_MIN_LEN
+        PASS_MIN_LEN_RESULT = bsf.grep_shell(self.config['query']['form'][2],self.config['query']['path'])[0]
+        if len(PASS_MIN_LEN_RESULT)!=0:
             bsf.sed_shell(bsf.grep_shell(self.config['query']['form'][2],self.config['query']['path'])[0],self.config['change']['value'][2],self.config['query']['path']) 
         else:
             bsf.append_line(self.config['change']['value'][2],self.config['query']['path'])
-        if len(bsf.grep_shell(self.config['query']['form'][3],self.config['query']['path'])[0])!=0:
+        
+        # PASS_WARN_AGE
+        PASS_WARN_AGE_RESULT = bsf.grep_shell(self.config['query']['form'][3],self.config['query']['path'])[0]
+        if len(PASS_WARN_AGE_RESULT)!=0:
             bsf.sed_shell(bsf.grep_shell(self.config['query']['form'][3],self.config['query']['path'])[0],self.config['change']['value'][3],self.config['query']['path']) 
         else:
             bsf.append_line(self.config['change']['value'][3],self.config['query']['path'])
+        
         data='type:fix,des:{}'.format(self.config['description'])
         logging.info(data)
         self.finalfix()
